478da8db601710ae76d4ef71b5172324722a3564
Sebastian Cano <sebastiancano12560@gmail.com>
2026-08-03 11:40:01 -0500

    feat aaa

--- backend/src/main/java/co/com/fitel/modules/pqr/infrastructure/soap/WebServiceConfig.java
+++ backend/src/main/java/co/com/fitel/modules/pqr/infrastructure/soap/WebServiceConfig.java
@@ -9,6 +9,7 @@ import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.ws.wsdl.wsdl11.SimpleWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
 
@@ -64,14 +65,8 @@ public class WebServiceConfig extends WsConfigurerAdapter {
      * del fichero WSDL publicado.</p>
      */
     @Bean(name = "consultaCUN")
-    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema pqrConsultaSchema) {
-        DefaultWsdl11Definition definition = new DefaultWsdl11Definition();
-        definition.setPortTypeName("WSConsultaOperadorPort");
-        definition.setLocationUri("/ws");
-        definition.setTargetNamespace(NAMESPACE_URI);
-        definition.setSchema(pqrConsultaSchema);
-        definition.setRequestSuffix("");
-        return definition;
+    public SimpleWsdl11Definition defaultWsdl11Definition() {
+        return new SimpleWsdl11Definition(new ClassPathResource("wsdl/consultaCUN.wsdl"));
     }
 
     /**
